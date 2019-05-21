75cba5545c85176d46d7df1d1e1b38c92095536e
Abbie Ke <abbieke222@gmail.com>
2019-05-21 18:12:33 +0800

    Register

---
+++ MvcProjectTest/Models/Customer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjectTest.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Display(Name = "會員姓名")]
+        [Required]
+        [StringLength(20,MinimumLength =1,ErrorMessage ="必填欄位")]
+        public string CustomerName { get; set; }
+
+        [Display(Name = "連絡電話")]
+        [Required]
+        [RegularExpression(@"^\d{4}\-?\d{3}\-?\d{3}$",ErrorMessage ="需為09xx-xxx-xxx")]
+        public string CustomerPhone { get; set; }
+
+        [Display(Name = "會員生日")]
+        [Required(ErrorMessage = "請選擇生日")]
+        [DataType(DataType.Date)]
+        public DateTime CustomerBirth { get; set; }
+
+        [Display(Name = "地址")]
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "必填欄位")]
+        public string CustomerAddress { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress]
+        [Required(ErrorMessage = "請輸入Email")]
+        [DataType(DataType.EmailAddress)]
+        public string CustomerEmail { get; set; }
+
+        [Display(Name = "會員帳號")]
+        [Required]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "最少需要8個字元")]
+        public string CustomerAccount { get; set; }
+
+        [Display(Name ="會員密碼")]
+        [Required]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,30}$", ErrorMessage = "至少有一個數字、小寫英文字母、大寫英文字母")]
+        public string CustomerPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "確認密碼")]
+        [Compare("CustomerPassword", ErrorMessage = "密碼和確認密碼不相符")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "帳號")]
+        public string CustomerAccount { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密碼")]
+        public string CustomerPassword { get; set; }
+
+        [Display(Name = "記住我?")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ MvcProjectTest/Repositories/CustomersRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using MvcProjectTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjectTest.Repositories
+{
+    public class CustomersRepository
+    {
+        private static string connString;
+        private readonly SqlConnection conn;
+        public CustomersRepository()
+        {
+            if (string.IsNullOrEmpty(connString))
+            {
+                connString = ConfigurationManager.ConnectionStrings["bsmobile"].ConnectionString;
+            }
+
+            conn = new SqlConnection(connString);
+        }
+
+        public void InsertCustomer(Customer cust)
+        {
+            using (conn)
+            {
+                string sql = "INSERT INTO Customers(CustomerName, CustomerAccount, CustomerPassword, CustomerEmail, CustomerPhone, CustomerAddress, CustomerBirth) VALUES ( @CustomerName, @CustomerAccount, @CustomerPassword, @CustomerEmail, @CustomerPhone, @CustomerAddress, @CustomerBirth)";
+                conn.Execute(sql, new { cust.CustomerName,cust.CustomerAccount, cust.CustomerPassword, cust.CustomerEmail, cust.CustomerPhone, cust.CustomerAddress, cust.CustomerBirth });
+            }
+        }
+    }
+}
